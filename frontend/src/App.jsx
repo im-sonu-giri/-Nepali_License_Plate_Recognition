16b27b4da9b215ed4669a6bb35542887164b8152
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-24 22:53:02 +0545

    Making landing page better

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './components/header.jsx'
 import ImageUpload from './pages/ImageUpload.jsx';
+import Home from './pages/Home.jsx';
 import './App.css'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
       <main style={{ marginLeft: '240px', padding: '20px', width: '100%' }}>
         <ImageUpload />
       </main>

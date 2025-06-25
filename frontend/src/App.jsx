af079bc91b2fa96f785176302c1b456e21964f9d
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-25 18:59:36 +0545

    Home page code writing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import Header from './components/header.jsx'
 import ImageUpload from './pages/ImageUpload.jsx';
-import Home from './pages/Home.jsx';
 import './App.css'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Home />
       <main style={{ marginLeft: '240px', padding: '20px', width: '100%' }}>
         <ImageUpload />
       </main>

391d38aade9237efeea641c01a1cfaa991c1072b
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-26 22:41:38 +0545

    app jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import Header from './components/header.jsx'
-import ImageUpload from './pages/ImageUpload.jsx';
+import ImageUpload from './pages/ImageUpload.jsx'
+import ResultPage from './pages/ResultPage.jsx'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <main style={{ marginLeft: '330px', padding: '18px', width: '110%' }}>
-        <ImageUpload />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<ImageUpload />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
       </main>
     </div>
   );

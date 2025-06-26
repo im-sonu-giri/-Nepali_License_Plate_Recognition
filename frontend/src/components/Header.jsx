946c2092aa3839c8891f0b8b75b417a78f0bc0c6
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-26 21:34:03 +0545

    upload container width adjusted

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import img from "../assets/image.png";
 
 export default function Header() {
     return(
         <header className="header">
-            <div className="logo">NLPR System</div>
-            <div className="subtitle">Nepali License Plate Recognition</div>
+            <div className="logo">Nepali License Plate Recognition</div>
+            <div className="subtitle">NLPR System</div>
             
-            <nav className="nav-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/recognize" className="nav-link active">Recognize</Link>
-            </nav>
+            <img src={img} className="logo" alt="logo" />
             
             <div className="footer">
                 <div className="copyright">© 2024 NLPR System</div>

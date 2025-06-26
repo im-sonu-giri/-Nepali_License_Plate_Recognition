028966c45ebd5a7f1c1615b3fbb7700aa1f0b84d
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-26 22:29:31 +0545

    Header part completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,11 +7,19 @@ export default function Header() {
         <header className="header">
             <div className="logo">Nepali License Plate Recognition</div>
             <div className="subtitle">सवारी साधन नम्बर प्लेट पहिचान प्रणाली</div>
-            
+            <hr className="divider" />
             <img src={img} className="img" alt="logo" />
-            
+            <div className="project-intro">
+                <p>
+                   नेपाली नम्बर प्लेट चिन्न र पढ्न तयार गरिएको स्वचालित प्रणाली।<br />
+                   Built to detect and recognize Nepali license plates.
+                </p>  
+            </div>
+
+            {/* <hr className="divider" /> */}
             <div className="footer">
-                <div className="copyright">© 2024 NLPR System</div>
+                
+                <div className="copyright">© 2024 NLPR System (Team 200_OK)</div>
                 <div className="institution">Nepal College of Information Technology</div>
             </div>
         </header>

c284cffcd3124bbe242696e6d2fae03b2b2a37d4
[Astha Thapa] <[astha.221707@ncit.edu.np]>
2025-06-27 22:41:03 +0545

    Checking aboutpage to imgupload scrollability test via uppdating css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header.jsx'
 import ImageUpload from './pages/ImageUpload.jsx'
 import ResultPage from './pages/ResultPage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
+import HomePage from './pages/HomePage.jsx'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <main style={{ marginLeft: '310px', padding: '18px', width: '110%' }}>
         <Routes>
-          <Route path="/" element={<ImageUpload />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/result" element={<ResultPage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes> 

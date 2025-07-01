af7ff90c0028831f08af5eeabb3c97317f16712c
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-01 15:43:58 +0545

    Add image upload and POST request to backend

--- frontend/src/components/UploadPlate.jsx
+++ frontend/src/components/UploadPlate.jsx
@@ -9,4 +9,25 @@ export default function UploadPlate()
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
     };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!file) {
+            alert("please select an image file");
+            return
+        }
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try{
+            const response = await axios.post("http://127.0.0.1:8000/detect", formData ,{
+                headers:{
+                    "content-Type": "multipart/form-data",
+                },
+            });
+            setResult(response.data);
+        }
+        
+    }
 }

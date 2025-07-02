3a70293183deab848b4e9c695e7027ab6681500e
im-sonu-giri <124750682+im-sonu-giri@users.noreply.github.com>
2025-07-02 22:27:39 +0545

    add UploadPlate component with file input and state management

--- frontend/src/components/UploadPlate.jsx
+++ frontend/src/components/UploadPlate.jsx
@@ -1,2 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+
+export default function UploadPlate()
+{
+    const [file , setFile ]= useState(null);
+    const [result, setResult] = useState(null);
+    const handleFileChange = (e) =>{
+        setFile(e.target.files[0]);
+    };
+}
